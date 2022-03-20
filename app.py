fe8cbc24591bd7daa97e5b965b4f0bf8d295a844
Angela Hu <angelahu925@gmail.com>
2022-03-19 21:05:52 -0700

    Added simple leaderboard

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
-import os
+import leaderboard
 
 app = Flask(__name__)
-app.secret_key = os.urandom(12).hex()
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    intern_scores = leaderboard.get_scores()
+    return render_template("index.html", intern_scores=intern_scores)
 
 
 if __name__ == "__main__":
---
+++ leaderboard.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+import os.path
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+SCOPES = ['https://www.googleapis.com/auth/classroom.coursework.students',
+          'https://www.googleapis.com/auth/classroom.rosters', 'https://www.googleapis.com/auth/classroom.courses']
+
+
+def get_scores():
+
+    coursework_array = []
+    student_array = []
+    course_id = ""
+
+    # Authorize user for Google Classroom API
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json')
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('classroom', 'v1', credentials=creds)
+    course_list = service.courses()
+    # Get courses
+    courses = course_list.list().execute()
+    for course in courses["courses"]:
+        if "E1T1 Spring" in course["name"]:
+            course_id = course["id"]
+    # Get students
+    students = service.courses().students().list(courseId=course_id).execute()
+    for student in students["students"]:
+        student_dict = {"id": student["userId"], "name": student["profile"]["name"]["givenName"], "score": 0}
+        student_array.append(student_dict)
+
+    # Get all courseWork
+    assignments = course_list.courseWork().list(courseId=course_id).execute()
+    for assignment in assignments["courseWork"]:
+        coursework_array.append(assignment["id"])
+
+    # Get students scores from work
+    for coursework in coursework_array:
+        grades = course_list.courseWork().studentSubmissions().list(courseId=course_id,
+                                                                    courseWorkId=coursework).execute()
+        for grade in grades["studentSubmissions"]:
+            try:
+                for intern in student_array:
+                    if intern["id"] == grade["userId"]:
+                        intern["score"] += int(grade["assignedGrade"])
+            except:
+                pass
+
+    return student_array
+
